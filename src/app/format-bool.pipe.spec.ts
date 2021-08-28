7d4f0e43ce83ef0adf02482dc1f830ccb63d1d57
Damir Arh <damir.arh@gmail.com>
2021-08-28 15:06:15 +0200

    Angular can't render a Promise

--- src/app/format-bool.pipe.spec.ts
+++ src/app/format-bool.pipe.spec.ts
@@ -1,8 +1,11 @@
+import { TranslateService } from '@ngx-translate/core';
 import { FormatBoolPipe } from './format-bool.pipe';
 
 describe('FormatBoolPipe', () => {
   it('create an instance', () => {
-    const pipe = new FormatBoolPipe();
+    const pipe = new FormatBoolPipe(
+      jasmine.createSpyObj<TranslateService>(['get'])
+    );
     expect(pipe).toBeTruthy();
   });
 });
--- src/app/format-bool.pipe.ts
+++ src/app/format-bool.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
   name: 'formatBool',
 })
 export class FormatBoolPipe implements PipeTransform {
-  transform(value: boolean): string {
-    return value ? 'BOOLEAN.TRUE' : 'BOOLEAN.FALSE';
+  constructor(private readonly translate: TranslateService) {}
+
+  transform(value: boolean): Promise<string> {
+    const translationKey = value ? 'BOOLEAN.TRUE' : 'BOOLEAN.FALSE';
+    return this.translate.get(translationKey).toPromise();
   }
 }
